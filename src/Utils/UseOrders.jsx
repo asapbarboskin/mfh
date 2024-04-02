96933d8fcb000fd8cf1fc47b597783ace3f0095a
islamabdrakhmanov <asapbarboskin@gmail.com>
2024-04-02 12:24:50 +0500

    Add localStorage API  for managing shopping cart data

--- src/Utils/UseOrders.jsx
+++ src/Utils/UseOrders.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function UseOrders() {
-	const [orders, setOrders] = useState([])
+	const getOrdersFromStorage = () => {
+		const storedOrders = localStorage.getItem('orders')
+		return storedOrders ? JSON.parse(storedOrders) : []
+	}
+
+	const updateOrdersInStorage = newOrders => {
+		localStorage.setItem('orders', JSON.stringify(newOrders))
+	}
+
+	const [orders, setOrders] = useState(getOrdersFromStorage)
 
 	const DeleteOrder = id => {
-		setOrders(prevOrders => prevOrders.filter(order => order.id !== id))
+		setOrders(prevOrders => {
+			const updatedOrders = prevOrders.filter(order => order.id !== id)
+			updateOrdersInStorage(updatedOrders)
+			return updatedOrders
+		})
 	}
 
 	const AddToCheckout = newOrder => {
-		let isOrderExist = false
-		orders.forEach(order => {
-			if (order.id === newOrder.id) isOrderExist = true
+		setOrders(prevOrders => {
+			let isOrderExist = false
+			prevOrders.forEach(order => {
+				if (order.id === newOrder.id) isOrderExist = true
+			})
+			if (!isOrderExist) {
+				const updatedOrders = [...prevOrders, newOrder]
+				updateOrdersInStorage(updatedOrders)
+				return updatedOrders
+			} else {
+				return prevOrders
+			}
 		})
-		if (!isOrderExist) {
-			setOrders(prevOrders => [...prevOrders, newOrder])
-		}
 	}
+
+	useEffect(() => {
+		updateOrdersInStorage(orders)
+	}, [orders])
 
 	return { orders, AddToCheckout, DeleteOrder }
 }
